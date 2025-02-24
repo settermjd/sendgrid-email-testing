fd5c419046a5af04c2c6470003ec80b21e383144
Matthew Setter <matthew@matthewsetter.com>
2025-02-24 16:46:48 +1000

    Add tests for the code
    This change is reasonably substantial as it adds covering tests for the
    code and refactors it along the way to pass those tests.

--- src/Handler/SendEmailHandler.php
+++ src/Handler/SendEmailHandler.php
@@ -4,19 +4,26 @@ declare(strict_types=1);
 
 namespace App\Handler;
 
-use Exception;
 use Laminas\Diactoros\Response\EmptyResponse;
 use Laminas\Diactoros\Response\JsonResponse;
+use Laminas\Diactoros\Response\TextResponse;
+use PH7\JustHttp\StatusCode;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\RequestHandlerInterface;
 use SendGrid;
 use SendGrid\Mail\Mail;
+use SendGrid\Mail\TypeException;
 
-use function array_filter;
-use function printf;
+use function array_diff;
+use function array_intersect;
+use function array_keys;
+use function count;
+use function json_decode;
 
-class SendEmailHandler implements RequestHandlerInterface
+use const JSON_OBJECT_AS_ARRAY;
+
+readonly class SendEmailHandler implements RequestHandlerInterface
 {
     public const array REQUIRED_POST_PARAMS = [
         'content_html',
@@ -27,45 +34,73 @@ class SendEmailHandler implements RequestHandlerInterface
         'to_name',
     ];
 
+    public function __construct(private Mail $mail, private SendGrid $sendGrid)
+    {
+    }
+
     public function handle(ServerRequestInterface $request): ResponseInterface
     {
-        $parsedBody = $request->getParsedBody();
-        $keys = array_intersect(self::REQUIRED_POST_PARAMS, array_keys($parsedBody));
+        $parsedBody = (array) $request->getParsedBody();
+        $keys       = array_intersect(self::REQUIRED_POST_PARAMS, array_keys($parsedBody));
         if (count($keys) !== count(self::REQUIRED_POST_PARAMS)) {
             return new JsonResponse(
                 [
-                    "Error" => "Missing configuration items.",
+                    "Error"                        => "Missing configuration items.",
                     "Missing configuration items." => array_diff(
                         self::REQUIRED_POST_PARAMS,
                         array_keys($parsedBody)
-                    )
+                    ),
                 ]
             );
         }
-        $this->sendEmail($parsedBody);
-        return new EmptyResponse();
+
+        try {
+            $response = $this->sendEmail($parsedBody);
+        } catch (TypeException $e) {
+            return new TextResponse($e->getMessage(), StatusCode::BAD_REQUEST);
+        }
+
+        return match ($response->statusCode()) {
+            StatusCode::ACCEPTED => new EmptyResponse(),
+            StatusCode::BAD_REQUEST,
+            StatusCode::FORBIDDEN,
+            StatusCode::INTERNAL_SERVER_ERROR,
+            StatusCode::METHOD_NOT_ALLOWED,
+            StatusCode::NOT_FOUND,
+            StatusCode::PAYLOAD_TOO_LARGE,
+            StatusCode::UNAUTHORIZED => new TextResponse(
+                $this->getErrorMessage($response),
+                StatusCode::UNAUTHORIZED
+            ),
+            default => new TextResponse("Unknown response", StatusCode::INTERNAL_SERVER_ERROR),
+        };
     }
 
-    private function sendEmail(array $config): void
+    /**
+     * sendEmail simplifies initialising the Mail object and sending it.
+     *
+     * @param array<string,string> $config The mail parameters for the sender, recipient, subject, and HTML body
+     * @throws TypeException
+     */
+    private function sendEmail(array $config = []): SendGrid\Response
     {
-        $email = new Mail();
+        $this->mail->setFrom($config['from_address'], $config['from_name']);
+        $this->mail->addTo($config['to_address'], $config['to_name']);
+        $this->mail->setSubject($config['subject']);
+        $this->mail->addContent('text/html', $config['content_html']);
 
-        $email->setFrom($config['from_address'], $config['from_name']);
-        $email->addTo($config['to_address'], $config['to_name']);
-        $email->setSubject($config['subject']);
-        $email->addContent('text/html', $config['content_html']);
+        return $this->sendGrid->send($this->mail);
+    }
 
-        $sendgrid = new SendGrid($_ENV['SENDGRID_API_KEY']);
-        try {
-            $response = $sendgrid->send($email);
-            printf("Response status: %d\n\n", $response->statusCode());
-            $headers = array_filter($response->headers());
-            echo "Response Headers\n\n";
-            foreach ($headers as $header) {
-                echo '- ' . $header . "\n";
-            }
-        } catch (Exception $e) {
-            echo 'Caught exception: ' . $e->getMessage() . "\n";
-        }
+    /**
+     * getErrorMessage returns the error message from the response body when the request results in an error
+     */
+    public function getErrorMessage(SendGrid\Response $response): string
+    {
+        return json_decode(
+            json: $response->body(),
+            associative: true,
+            flags: JSON_OBJECT_AS_ARRAY
+        )['errors']['message'];
     }
 }
--- test/Handler/SendEmailHandlerTest.php
+++ test/Handler/SendEmailHandlerTest.php
@@ -2,10 +2,180 @@
 
 declare(strict_types=1);
 
-namespace App\Handler;
+namespace AppTest\Handler;
 
+use App\Handler\SendEmailHandler;
+use Laminas\Diactoros\Response\EmptyResponse;
+use Laminas\Diactoros\Response\TextResponse;
+use PH7\JustHttp\StatusCode;
+use PHPUnit\Framework\Attributes\TestWith;
 use PHPUnit\Framework\TestCase;
+use Psr\Http\Message\ServerRequestInterface;
+use SendGrid;
+use SendGrid\Mail\Mail;
+use SendGrid\Mail\TypeException;
+
+use function json_encode;
 
 class SendEmailHandlerTest extends TestCase
 {
+    public function testSendEmail(): void
+    {
+        $requestBody = [
+            'from_address' => 'send.from@example.com',
+            'from_name'    => 'Sender',
+            'to_address'   => 'send.to@example.com',
+            'to_name'      => 'Recipient',
+            'subject'      => 'SendGrid Test Email',
+            'content_html' => '<p>Test</p>',
+        ];
+
+        $_ENV['SENDGRID_API_KEY'] = 'key';
+
+        $mail = $this->createMock(Mail::class);
+        $mail
+            ->expects($this->once())
+            ->method('setFrom')
+            ->with($requestBody['from_address'], $requestBody['from_name']);
+        $mail
+            ->expects($this->once())
+            ->method('addTo')
+            ->with($requestBody['to_address'], $requestBody['to_name']);
+        $mail
+            ->expects($this->once())
+            ->method('setSubject')
+            ->with($requestBody['subject']);
+        $mail
+            ->expects($this->once())
+            ->method('addContent')
+            ->with('text/html', $requestBody['content_html']);
+
+        $sendgrid = $this->createMock(SendGrid::class);
+        $sendgrid
+            ->expects($this->once())
+            ->method('send')
+            ->with($mail)
+            ->willReturn(new SendGrid\Response(
+                StatusCode::ACCEPTED,
+                '',
+                [
+                    'Content-Type' => 'application/json',
+                ]
+            ));
+
+        $handler = new SendEmailHandler($mail, $sendgrid);
+
+        $request = $this->createMock(ServerRequestInterface::class);
+        $request
+            ->expects($this->once())
+            ->method('getParsedBody')
+            ->willReturn($requestBody);
+
+        $response = $handler->handle($request);
+
+        $this->assertInstanceOf(EmptyResponse::class, $response);
+    }
+
+    #[TestWith([StatusCode::BAD_REQUEST, "invalid request"])]
+    #[TestWith([StatusCode::FORBIDDEN, "access forbidden"])]
+    #[TestWith([StatusCode::INTERNAL_SERVER_ERROR, "internal server error"])]
+    #[TestWith([StatusCode::METHOD_NOT_ALLOWED, "method not allowed"])]
+    #[TestWith([StatusCode::NOT_FOUND, "not found"])]
+    #[TestWith([StatusCode::PAYLOAD_TOO_LARGE, "content too large"])]
+    #[TestWith([StatusCode::UNAUTHORIZED, "authorization required"])]
+    public function testCanHandleErrors(int $statusCode, string $responseMessage): void
+    {
+        $requestBody = [
+            'from_address' => 'send.from@example.com',
+            'from_name'    => 'Sender',
+            'to_address'   => 'send.to@example.com',
+            'to_name'      => 'Recipient',
+            'subject'      => 'SendGrid Test Email',
+            'content_html' => '<p>Test</p>',
+        ];
+
+        $mail = $this->createMock(Mail::class);
+        $mail
+            ->expects($this->once())
+            ->method('setFrom')
+            ->with($requestBody['from_address'], $requestBody['from_name']);
+        $mail
+            ->expects($this->once())
+            ->method('addTo')
+            ->with($requestBody['to_address'], $requestBody['to_name']);
+        $mail
+            ->expects($this->once())
+            ->method('setSubject')
+            ->with($requestBody['subject']);
+        $mail
+            ->expects($this->once())
+            ->method('addContent')
+            ->with('text/html', $requestBody['content_html']);
+
+        $sendgrid = $this->createMock(SendGrid::class);
+        $sendgrid
+            ->expects($this->once())
+            ->method('send')
+            ->with($mail)
+            ->willReturn(new SendGrid\Response(
+                StatusCode::UNAUTHORIZED,
+                json_encode([
+                    'errors' => [
+                        "message" => "Unauthorized",
+                        "field"   => "null",
+                    ],
+                ]),
+                [
+                    'Content-Type' => 'application/json',
+                ]
+            ));
+
+        $handler = new SendEmailHandler($mail, $sendgrid);
+
+        $request = $this->createMock(ServerRequestInterface::class);
+        $request
+            ->expects($this->once())
+            ->method('getParsedBody')
+            ->willReturn($requestBody);
+
+        $response = $handler->handle($request);
+        $this->assertInstanceOf(TextResponse::class, $response);
+        $this->assertSame(StatusCode::UNAUTHORIZED, $response->getStatusCode());
+        $this->assertSame('Unauthorized', (string) $response->getBody());
+    }
+
+    public function testCanHandleSendGridExceptions(): void
+    {
+        $requestBody = [
+            'from_address' => 'send.from@example.com',
+            'from_name'    => 'Sender',
+            'to_address'   => 'not.an.email.address',
+            'to_name'      => 'Recipient',
+            'subject'      => 'SendGrid Test Email',
+            'content_html' => '<p>Test</p>',
+        ];
+
+        $errorMessage = '"$to_address" must be a valid email address. Got: not.an.email.address';
+
+        $mail = $this->createMock(Mail::class);
+        $mail
+            ->expects($this->once())
+            ->method('setFrom')
+            ->willThrowException(
+                new TypeException($errorMessage)
+            );
+
+        $request = $this->createMock(ServerRequestInterface::class);
+        $request
+            ->expects($this->once())
+            ->method('getParsedBody')
+            ->willReturn($requestBody);
+
+        $handler  = new SendEmailHandler($mail, $this->createMock(SendGrid::class));
+        $response = $handler->handle($request);
+
+        $this->assertInstanceOf(TextResponse::class, $response);
+        $this->assertSame(StatusCode::BAD_REQUEST, $response->getStatusCode());
+        $this->assertSame($errorMessage, (string) $response->getBody());
+    }
 }
